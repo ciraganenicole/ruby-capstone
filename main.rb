3fe9f2e183d5fca9aef6a15342dce28b11d90a11
Cédric Kossi <lionel.kossi@gmail.com>
2022-06-27 19:28:23 +0000

    Update options and add better architecture

--- main.rb
+++ main.rb
@@ -5,20 +5,17 @@ def main
     puts 'Welcome to : Catalog of my things ! :)'
     puts '1 - List all books'
     puts '2 - List all music albums'
-    puts '3 - List a movies'
-    puts '4 - List a games'
-    puts '5 - List a genres'
-    puts '6 - List a labels'
-    puts '7 - List a authors'
-    puts '8 - List a sources'
-    puts '9 - Add a book'
-    puts '10 - Add a music album'
-    puts '11 - Add a movie'
-    puts '12 - Add a game'
+    puts '3 - List of games'
+    puts '4 - List of genres'
+    puts '5 - List of authors'
+    puts '6 - Add a book'
+    puts '7 - Add a music album'
+    puts '8 - Add a game'
     puts '0 - Exit'
     input = gets.chomp.to_i
     Input.check_input(input)
   end
+  puts 'Thank you for using our service :)'
 end
 
 main
