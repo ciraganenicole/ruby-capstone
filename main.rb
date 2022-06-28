013fa027dbc172964676b73348176333af7f8440
Munezero Christian <chamsf5@aol.com>
2022-06-28 09:43:26 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
-require './input'
 require './app'
+require './input'
+require './storage'
 def main
   # Add some logic for the database
   app = App.new
@@ -10,11 +11,12 @@ def main
     puts '1 - List all books'
     puts '2 - List all music albums'
     puts '3 - List of games'
-    puts '4 - List of genres'
-    puts '5 - List of authors'
-    puts '6 - Add a book'
-    puts '7 - Add a music album'
-    puts '8 - Add a game'
+    puts '4 - List all genres'
+    puts '5 - List all labels'
+    puts '6 - List all authors'
+    puts '7 - Add a book'
+    puts '8 - Add a music album'
+    puts '9 - Add a game'
     puts '0 - Exit'
     input = gets.chomp.to_i
     Input.check_input(input, app)
