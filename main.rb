606fc70f57740a6dccd62e7b644e094240866578
Cédric Kossi <lionel.kossi@gmail.com>
2022-06-28 16:20:49 +0000

    Add storage

--- main.rb
+++ main.rb
@@ -6,9 +6,11 @@ def main
   app = App.new
   app.populate_app
   storage = Storage.new
+  storage.prepare_storage
   storage.load_data(app)
   loop do
     puts 'Welcome to : Catalog of my things ! :)'
+    puts 'Please chose an option : '
     puts '1 - List all books'
     puts '2 - List all music albums'
     puts '3 - List of games'
@@ -19,6 +21,7 @@ def main
     puts '8 - Add a music album'
     puts '9 - Add a game'
     puts '0 - Exit'
+    puts 'Enter your option : '
     input = gets.chomp.to_i
     Input.check_input(input, app)
   end
--- storage.rb
+++ storage.rb
@@ -1,3 +1,5 @@
+require './model/book'
+require 'json'
 class Storage
   attr_reader :files
 
@@ -14,9 +16,29 @@ class Storage
   # this method is called to save all data
   def save_data(app); end
 
-  # this method is called to load all data
-  def load_data(app); end
-
   # Implement your own logic to save your data
-  def save_book(app); end
+  def save_book(app)
+    return unless File.file?('books.json')
+
+    people_file = File.open('books.json', 'w')
+    people_file.write(JSON.generate(app.books))
+    people_file.close
+  end
+
+  # this method is called to load all data
+  def load_data(app)
+    puts 'Loading informations...'
+    load_book(app)
+  end
+
+  def load_book(app)
+    return unless File.file?('books.json')
+    return if File.zero?('books.json')
+
+    book_file = File.open('books.json', 'r')
+    book_list = JSON.parse(book_file.read)
+    book_list.each { |book| app.create_book(Book.new(cover_state: book['cover_state'], publisher: book['publisher'])) }
+    # puts app.books
+    book_file.close
+  end
 end
