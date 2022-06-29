8d6470bb458bbac804ea3a70fe90111f86b091a9
Munezero Christian <chamsf5@aol.com>
2022-06-29 18:08:09 +0200

    Revert "Music album"

--- app.rb
+++ app.rb
@@ -1,27 +1,23 @@
 require './model/label'
-require './model/music'
-require './model/genre'
 class App
-  attr_reader :genres, :authors, :labels, :books, :musics, :games
+  attr_reader :genres, :authors, :labels, :books, :music_album, :games
 
   def initialize
     @genres = []
     @authors = []
     @labels = []
     @books = []
-    @musics = []
+    @music_album = []
     @games = []
   end
 
   def populate_app
     labels = [Label.new(title: 'Drama', color: 'red'), Label.new(title: 'Fantasy', color: 'green')]
     labels.each { |label| @labels.push(label) }
+  end
 
-    genres = [Genre.new(name: 'Comedy'), Genre.new(name: 'Thriller'), Genre.new(name: 'Pop'),
-              Genre.new(name: 'Hip-hop and Rap'), Genre.new(name: 'Rock'),
-              Genre.new(name: 'Reggae'), Genre.new(name: 'Rhumba'), Genre.new(name: 'Country'),
-              Genre.new(name: 'Dance and Electronic'), Genre.new(name: 'Other')]
-    genres.each { |genre| @genres.push(genre) }
+  def add_genre(item)
+    # @genre.add_item(item)
   end
 
   def add_author(item)
@@ -41,22 +37,6 @@ class App
   def list_labels
     @labels.each_with_index do |label, index|
       puts "#{index}. [#{label.class}] - Title: #{label.title}, Color: #{label.color}"
-    end
-  end
-
-  # Add a music
-  def create_music(music)
-    @musics << music
-  end
-
-  # Genre part
-  def add_genre(item)
-    @genres.add_item(item)
-  end
-
-  def list_genres
-    @genres.each_with_index do |genre, index|
-      puts "#{index}. ID: #{genre.id} Name: #{genre.name}"
     end
   end
 end
