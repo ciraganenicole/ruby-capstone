e0bea147fa0991d0063f5f86185100898c1d625d
Munezero Christian <chamsf5@aol.com>
2022-06-30 11:18:07 +0200

    Add Schema.sql

--- schema.sql
+++ schema.sql
@@ -1,24 +1,56 @@
+CREATE DATABASE catalog;
 
--- CREATE TABLE BOOK
-
-CREATE TABLE IF NOT EXISTS book (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    publisher varchar(40),
-    cover_state varchar(40),
-    publish_date date,
-    archived bool
+CREATE TABLE item (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    genre_id INT,
+    author_id INT,
+    label_id INT,
+    publish_date DATE,
+    archived BOOLEAN,
+    PRIMARY KEY(id),
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (author_id) REFERENCES authors (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id)
 );
 
-CREATE TABLE label (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    title varchar(40),
-    color varchar(40)
+CREATE TABLE genres (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    PRIMARY KEY(id)
 );
 
-ALTER TABLE book
-ADD COLUMN label INT,
-ADD FOREIGN KEY (id) REFERENCES label(id),
-ADD COLUMN genre INT,
-ADD FOREIGN KEY (id) REFERENCES genre(id),
-ADD COLUMN author INT,
-ADD FOREIGN KEY (id) REFERENCES author(id);
+CREATE TABLE authors (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE labels (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(100),
+    color VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE books (
+    id  INT,
+    title VARCHAR(100),
+    publisher VARCHAR(100),
+    cover_state VARCHAR(100),
+    FOREIGN KEY(id) REFERENCES item(id)
+);
+
+CREATE TABLE music_albums (
+    id  INT,
+    name VARCHAR(100),
+    on_spotify BOOLEAN,
+    FOREIGN KEY(id) REFERENCES item(id)
+);
+
+CREATE TABLE games (
+    id  INT,
+    multiplayer BOOLEAN,
+    last_played_at DATE,
+    FOREIGN KEY(id) REFERENCES item(id)
+);
